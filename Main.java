249bb8de836abcba94d594881becfc613f2e9bb6
Mizab <mizabmansari@gmail.com>
2023-02-15 22:13:50 +0530

    Extra changes

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String... s) {
-        System.out.println("Lol");
-        System.out.println("lmao");
-        System.out.println("Mizab=chamdi");
+        System.out.println("lmaoooooasdiasndfabsdfhj");
     }
 }
